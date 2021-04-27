6e4de18d05ea566140e9f093ba442a8e7e69e231
Selton Guedes <sfgs@ecomp.poli.br>
2021-04-27 06:15:38 -0300

    Object and Array types

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(resquest: Request, response: Response) {
+  const user = createUser({
+    email: 'email@mail.com',
+    password: '123',
+    techs: [
+      'Node.js',
+      'ReactJS',
+      'React native',
+      { title: 'Javascript', experience: 100 }
+    ],
+  });
+
   return response.json({ message: "Hello World" });
 };
